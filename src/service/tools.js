ebdb213d6ea36b060d22ac725d55e48a866429fa
Geraldopereirads <geraldo.pereirads@hotmail.com>
2025-05-07 11:08:55 -0300

    feat: add contact page

---
+++ src/service/tools.js
@@ -0,0 +1,67 @@
+export const tool = [
+    {
+        id: "1",
+        title: "HTML"
+    },
+    {
+        id: "2",
+        title: "CSS"
+    },
+    {
+        id: "3",
+        title: "JavaScript"
+    },
+    {
+        id: "4",
+        title: "React"
+    },
+    {
+        id: "5",
+        title: "NodeJs"
+    },
+    {
+        id: "6",
+        title: "Next.js"
+    },
+    {
+        id: "7",
+        title: "TailwindCSS"
+    },
+    {
+        id: "8",
+        title: "Prisma"
+    },
+    {
+        id: "9",
+        title: "PostgreSQL"
+    },
+    {
+        id: "10",
+        title: "AWS"
+    },
+    {
+        id: "11",
+        title: "Git"
+    },
+    {
+        id: "12",
+        title: "GitHub"
+    },
+    {
+        id: "13",
+        title: "Figma"
+    },
+    {
+        id: "14",
+        title: "C#"
+    },
+    {
+        id: "15",
+        title: ".NET"
+    },
+    {
+        id: "16",
+        title: "SQL"
+    }
+ 
+]
